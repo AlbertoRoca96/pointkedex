94035704f2c8e6a9ae5028a06457bb2c0ab23dcb
AlbertoRoca96 <alberto.roca@live.longwood.edu>
2025-07-26 19:52:48 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-// public API - used by app.js to POST /api/predict
-window.API_BASE = "https://88b9-123-45-67-8000.ngrok-free.app";
+(() => {
+  const urlParam = new URLSearchParams(window.location.search).get("api");
+  window.API_BASE = urlParam || "";   // "" == same origin
+})();
