7ec162fb68e212b711812d250f1d2f585c1daac2
AlbertoRoca96 <alberto.roca@live.longwood.edu>
2025-07-28 15:33:28 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 (() => {
   const urlParam = new URLSearchParams(window.location.search).get("api");
-  window.API_BASE = "https://your‑api.fly.dev/";
+  window.API_BASE = "https://pup-cloud-gydfae9fbcgddex.canadacentral-01.azurewebsites.net/";
+
 })();
