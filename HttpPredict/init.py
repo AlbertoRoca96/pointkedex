ac27ecda755f3b5a49ee58daee66cb62fcfefc35
AlbertoRoca96 <alberto.roca@live.longwood.edu>
2025-07-28 19:52:14 -0400

    Delete HttpPredict directory

--- HttpPredict/init.py
+++
@@ -1,5 +0,0 @@
-import azure.functions as func
-import predict_server as p      # Flask app is in repo root
-
-def main(req: func.HttpRequest, ctx: func.Context):
-    return func.WsgiMiddleware(p.app).handle(req, ctx)
