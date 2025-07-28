e14515d6caffe750f0395a9079726ce9d4eb047c
AlbertoRoca96 <AlbertoRoca96@users.noreply.github.com>
2025-07-28 20:53:58 +0000

    deploy: 9182477aa690d6e22780a8548a3384f577c57f9a

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 (() => {
   const urlParam = new URLSearchParams(window.location.search).get("api");
-  window.API_BASE = "https://pup-cloud-pupcloud-a2hkd4hmc9g6ddej.canadacentral-01.azurewebsites.net/";
+  window.API_BASE = "pup-cloud-gvfdfae9fbcqddex.canadacentral-01.azurewebsites.net/";
 
 
 
